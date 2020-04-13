ec6c75daa1479d3eb5c223d7aa4e0e09e576e479
Nalin Das <nalindas9@gmail.com>
2020-04-13 15:57:30 -0400

    Add references

--- 2D_space.py
+++ 2D_space.py
@@ -1,4 +1,4 @@
-import 
+import env
 
 # Defining Initial State
 ZOMBIE = 'z'
@@ -11,4 +11,14 @@ grid = [[ICE_CREAM, EMPTY],[ZOMBIE, CAR]]
 for row in grid:
   print(''.join(row))
   
-  
+# Defining actions
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
+ACTIONS = [UP, DOWN, LEFT, RIGHT]
+
+start_state = env.State(grid, [1,1])
+
+
